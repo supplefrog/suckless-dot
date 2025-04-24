361ff57c13f424120f64bb9395d9a8625f99bb0d
supplefrog <78985073+supplefrog@users.noreply.github.com>
2025-04-24 11:28:47 +0530

    Delete home/e/de directory

--- home/e/de/dwm/de.sh
+++
@@ -1,6 +0,0 @@
-(while true; do feh --bg-fill --randomize ~/de/feh/bg/*; sleep 30; done) &
-while true; do
-    xsetroot -name "$(date '+%H:%M')"
-    sleep 1
-done &
-exec dwm
--- home/e/de/feh/scripts/update-todo.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh -e
-
-ghi --repo=derf/feh --verbose list > TODO
-git commit -m 'Update TODO (via github)' TODO
