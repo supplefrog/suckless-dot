6036563cedcf74b57d9cbab59a16eb8848ba2255
supplefrog <78985073+supplefrog@users.noreply.github.com>
2025-04-24 11:14:13 +0530

    Update install_deps.sh

--- install_deps.sh
+++ install_deps.sh
@@ -7,8 +7,6 @@ detect_pkg_mgr
 # List of packages to install
 PKG_LIST="epel-release xorg xorg-dev xserver-xorg libcurl4-openssl-dev libimlib2-dev libx11-dev libxft-dev libxinerama-dev libxrandr-dev libxcb1-dev libxt-dev gcc git make pkg-config dmenu vifm"
 
-echo "Installing required packages..."
-
 for pkg in $PKG_LIST; do
     echo "Installing $pkg..."
     if ! $INSTALL_CMD "$pkg"; then
