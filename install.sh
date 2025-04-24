cd95480bc83318aeb8f311c1fdf1561d4987751f
supplefrog <78985073+supplefrog@users.noreply.github.com>
2025-04-24 11:13:28 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,10 +21,8 @@ REPO_BUILDS=(
 )
 
 echo "Installing dependencies..."
-set +e
 source "$(dirname "$0")/install_deps.sh"
 source "$(dirname "$0")/install_deps_src.sh"
-set -e
 
 echo "Copying dot files..."
 source "$(dirname "$0")/dot.sh"
