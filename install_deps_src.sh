0dc915ab4a55f32dc3998fd493819070d65c45d9
supplefrog <78985073+supplefrog@users.noreply.github.com>
2025-04-25 18:47:46 +0530

    Update install_deps_src.sh

--- install_deps_src.sh
+++ install_deps_src.sh
@@ -7,32 +7,34 @@ git_version=$(git --version | awk '{print $3}')
 required_version="2.27"
 
 # Function to compare versions
+# Arguments: version1, version2
+# Print 1 if version1 > version2, -1 if version1 < version2, 0 if equal
 version_compare() {
-    # Arguments: version1, version2
-    # Return 1 if version1 > version2, -1 if version1 < version2, 0 if equal
     if [[ "$1" == "$2" ]]; then
-        return 0
+        echo 0
+        return
     fi
     local IFS="."
     local i v1=($1) v2=($2)
     for ((i=0; i < ${#v1[@]} || i < ${#v2[@]}; i++)); do
         if [[ ${v1[i]:-0} -gt ${v2[i]:-0} ]]; then
-            return 1
+            echo 1
+            return
         elif [[ ${v1[i]:-0} -lt ${v2[i]:-0} ]]; then
-            return -1
+            echo -1
+            return
         fi
     done
-    return 0
+    echo 0
 }
 
 # Compare current git version to required version
-version_compare "$git_version" "$required_version"
-result=$?
+result=$(version_compare "$git_version" "$required_version")
 
 # If the installed version is less than the required version (result is -1)
-if [ $result -eq -1 ]; then
+if [ "$result" -eq -1 ]; then
     echo "Git version is lower than 2.27. Installing Git 2.27..."
-    
+
     # Install required build tools
     sudo apt update
     sudo apt install -y build-essential libcurl4-openssl-dev libexpat1-dev gettext libz-dev
@@ -48,11 +50,10 @@ if [ $result -eq -1 ]; then
     # Compile and install
     make prefix=/usr/bin all
     sudo make prefix=/usr/bin install
-    
+
     # Clean up
     rm -rf /tmp/git
     echo "Git 2.27 installed successfully."
 else
     echo "Git version is $git_version. No update needed."
 fi
-
