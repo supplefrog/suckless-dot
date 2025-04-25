93a7405485371033a3eae8451bbfd5aeee27e67b
supplefrog <78985073+supplefrog@users.noreply.github.com>
2025-04-25 17:07:34 +0530

    Update de.sh

--- home/e/.de/dwm/de.sh
+++ home/e/.de/dwm/de.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-(while true; do feh --bg-fill --randomize $HOME/.de/feh/bg/*; sleep 30; done) &
+while true; do feh --bg-fill --randomize $HOME/.de/feh/bg/*; sleep 30; done &
 
-(while true; do xsetroot -name "$(date '+%H:%M')"; sleep 60; done) &
+while true; do xsetroot -name "$(date '+%H:%M')"; sleep 60; done &
 
 exec dwm
