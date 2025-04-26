e5fce9d4b44459ef0ce0de7f9bd39db274dea152
supplefrog <78985073+supplefrog@users.noreply.github.com>
2025-04-26 11:19:09 +0530

    Update install_deps_src.sh

--- install_deps_src.sh
+++ install_deps_src.sh
@@ -41,7 +41,7 @@ if [ "$result" -eq -1 ]; then
 
     # Clone Git repository
     cd /tmp
-    clone_repos "https://github.com/git/git.git"  # Your custom function
+    clone_repos "https://github.com/git/git.git"
     cd git
 
     # Checkout the version 2.27
