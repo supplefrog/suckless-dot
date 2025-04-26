f4f87ee23c70bc2d1923fb6c7ee7b665c81fe315
supplefrog <78985073+supplefrog@users.noreply.github.com>
2025-04-26 12:58:08 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -31,7 +31,7 @@ for i in "${!REPO_DIRS[@]}"; do
         for PATCH in st-anysize-20220718-baa9357.diff st-scrollback-ringbuffer-0.9.2.diff st-scrollback-float-0.9.2.diff; do
             if [[ -f "$PATCH" ]]; then
                 echo "Applying patch: $PATCH"
-                patch -p1 < "$PATCH" -N
+                patch -p1 < "$PATCH" -N || echo ""
             fi
         done
         cp config.def.h config.h
