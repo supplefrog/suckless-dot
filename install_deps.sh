ac3cf5f2f59011fc935a91dbba91e515713e16c7
supplefrog <78985073+supplefrog@users.noreply.github.com>
2025-04-28 19:23:02 +0530

    Update install_deps.sh

--- install_deps.sh
+++ install_deps.sh
@@ -8,14 +8,14 @@ detect_pkg_mgr
 # Set PKG_LIST based on the package manager
 case "$PKG_MGR" in
     "apt")
-        PKG_LIST="build-essential libx11-dev libxft-dev libxinerama-dev libxrandr-dev libxcb1-dev libxt-dev libcurl4-openssl-dev libimlib2-dev libfreetype6-dev fontconfig"
+        PKG_LIST="build-essential libx11-dev libxft-dev libxinerama-dev libxrandr-dev libxcb1-dev libxt-dev libimlib2-dev libfreetype6-dev fontconfig"
         ;;
     "dnf" | "yum")
-        PKG_LIST="gcc make pkg-config libX11-devel libXft-devel libXinerama-devel libXrandr-devel libxcb-devel libXt-devel libcurl-devel imlib2-devel freetype-devel fontconfig-devel"
+        PKG_LIST="gcc make pkg-config libX11-devel libXft-devel libXinerama-devel libXrandr-devel libxcb-devel libXt-devel imlib2-devel freetype-devel fontconfig-devel"
         INSTALL_CMD="$PKG_MGR groupinstall 'Development Tools' & $INSTALL_CMD"
         ;;
     "pacman")
-        PKG_LIST="base-devel libx11 libxft libxinerama libxrandr libxcb libxt curl imlib2 freetype2 fontconfig"
+        PKG_LIST="base-devel libx11 libxft libxinerama libxrandr libxcb libxt imlib2 freetype2 fontconfig"
         ;;
 esac
 
