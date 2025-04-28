760a31f1d3120ab1d1076aa569be7c9614f6029a
supplefrog <78985073+supplefrog@users.noreply.github.com>
2025-04-28 12:26:36 +0530

    Update install_deps.sh

--- install_deps.sh
+++ install_deps.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 detect_pkg_mgr
 
 # List of packages to install
-PKG_LIST="epel-release xorg xorg-dev xserver-xorg libcurl4-openssl-dev libimlib2-dev libx11-dev libxft-dev libxinerama-dev libxrandr-dev libxcb1-dev libxt-dev gcc git make pkg-config dmenu vifm"
+PKG_LIST="epel-release xorg xorg-dev xserver-xorg libcurl4-openssl-dev libimlib2-dev libx11-dev libxft libxft-dev libXft-devel libxinerama-dev libxrandr-dev libxcb1-dev libxt-dev gcc git make pkg-config dmenu vifm"
 
 echo "Installing required packages..."
 $INSTALL_CMD $PKG_LIST || echo "Warning: Some packages may have failed to install."
