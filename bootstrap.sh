dc056913fb9d6a6b46f73767fdb19563385455e8
supplefrog <78985073+supplefrog@users.noreply.github.com>
2025-04-28 12:46:09 +0530

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -16,15 +16,15 @@ detect_pkg_mgr() {
     if command -v apt &> /dev/null; then
         PKG_MGR="apt"
         INSTALL_CMD="sudo apt install -y"
-    elif command -v pacman &> /dev/null; then
-        PKG_MGR="pacman"
-        INSTALL_CMD="sudo pacman -Syu --noconfirm"
     elif command -v dnf &> /dev/null; then
         PKG_MGR="dnf"
         INSTALL_CMD="sudo dnf install -y"
     elif command -v yum &> /dev/null; then
         PKG_MGR="yum"
         INSTALL_CMD="sudo yum install -y"
+    elif command -v pacman &> /dev/null; then
+        PKG_MGR="pacman"
+        INSTALL_CMD="sudo pacman -Syu --noconfirm"
     else
         echo "Unsupported package manager."
         exit 1
