814fe1a36e5df293653e3ad3a63de2ce5f5f9775
supplefrog <78985073+supplefrog@users.noreply.github.com>
2025-06-03 14:22:33 +0530

    Delete home/e/.de/sxwm directory

--- home/e/.de/sxwm/build/parser.d
+++
@@ -1,5 +0,0 @@
-build/parser.o: src/parser.c src/parser.h src/defs.h
-
-src/parser.h:
-
-src/defs.h:
--- home/e/.de/sxwm/build/sxwm.d
+++
@@ -1,7 +0,0 @@
-build/sxwm.o: src/sxwm.c src/defs.h src/parser.h src/config.h
-
-src/defs.h:
-
-src/parser.h:
-
-src/config.h:
